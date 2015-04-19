2928c5fbe04ad6d9906b4b1beac18f61fc66acb3
Thierry Lagasse [Mac Pro] <akarun@krkn.be>
2015-04-19 21:21:46 +0200

    Add SSH complete

---
+++ ssh_complete.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+#   Autocompletor for SSH Hosts Connexion
+#
+#   Author: Thierry 'Epagneul' Lagasse <epagneul@dogstudio.be>
+#   Since: Apr 2015
+#
+# -----------------------------------------------------------------------------
+
+if [[ -n $(type -t completor) ]]; then
+    _complete() {
+        local word="${COMP_WORDS[COMP_CWORD]}"
+
+        COMPREPLY=( $(compgen -W "$(egrep 'Host\ .*' ~/.ssh/config | cut -f2 -d ' ')" -- "$word") )
+    }
+
+    complete -F _complete ssh
+fi
