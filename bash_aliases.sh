aa5e97b46ac435d00cba3c2e922e5e7bbfa75b8b
Thierry Lagasse [Mac Pro] <akarun@krkn.be>
2015-04-19 21:21:27 +0200

    Add Aliases

---
+++ bash_aliases.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+#   Aliases for generic commands
+#
+#   Author: Thierry 'Epagneul' Lagasse <epagneul@dogstudio.be>
+#   Since: Apr 2015
+#
+# -----------------------------------------------------------------------------
+
+alias ls='ls -G'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+
+alias l='clear; ls -la'
+alias duh='du -hs'
+alias tree="find . | sed 's/[^/]*\//|   /g;s/| *\([^| ]\)/+--- \1/'"
+alias wget="wget -c"
+
+alias ..='cd ..'
+alias ...='cd ../../'
+alias ....='cd ../../../'
