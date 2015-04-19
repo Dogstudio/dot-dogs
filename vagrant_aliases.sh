c0c144371fa4c9e5101b975bae56b9dac7ba6dcd
Thierry Lagasse [Mac Pro] <akarun@krkn.be>
2015-04-19 21:22:13 +0200

    Add Vagrant aliases

---
+++ vagrant_aliases.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+#   Aliases for Vagrant Commands and Paths
+#
+#   Author: Thierry 'Epagneul' Lagasse <epagneul@dogstudio.be>
+#   Since: Apr 2015
+#
+# -----------------------------------------------------------------------------
+
+PATH=$PATH:/Applications/VirtualBox.app/Contents/MacOS/
+PATH=$PATH:~/.composer/vendor/bin
+export PATH
+
+if [[ -z $(which vagrant) ]]; then
+
+    #
+    #   Vagrant InBox Shortcuts
+    #
+    function vagrant() {
+        case \$1 in
+            'halt')
+                sudo init 0
+                ;;
+            *)
+                echo "Oupss. You're in the VM..."
+                ;;
+        esac      
+    }
+
+    #
+    #   Vagrant Aliases
+    #    
+    alias vinstall='git archive --remote git@gitlab.dogstudio.be:devtools/vagrantdog.git master | tar -x -C ./'
+    alias vdestroy='vagrant destroy --force'
+    alias vup='vagrant up && vagrant ssh'
+    alias vrestart='vhalt && vup'
+    alias vssh='vagrant ssh'
+    alias vstate='vagrant global-status'
+    alias vlist='VBoxManage list runningvms'
+fi  
+
+#
+#   Vagrant Aliases (for inBox use)
+#
+alias vhalt='vagrant halt'
