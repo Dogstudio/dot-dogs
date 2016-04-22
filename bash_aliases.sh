2be7e362e3f960f720d5719ee1cbdb24ea59b52b
Thierry Lagasse <epagneul@dogstudio.be>
2016-04-22 09:23:14 +0200

    Udapte aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -17,9 +17,23 @@ alias duh='du -hs'
 alias tree="find . | sed 's/[^/]*\//|   /g;s/| *\([^| ]\)/+--- \1/'"
 alias wget="wget -c"
 
+
+# Quick Nav and Directory
+alias mkdir="mkdir -p"
 alias ..='cd ..'
 alias ...='cd ../../'
 alias ....='cd ../../../'
 alias .....='cd ../../../../'
 
+# PHP
 alias phpserv='php -S 0.0.0.0:8080 -t ./'
+
+# PHP Storm
+if [[ -n $(which pstorm) ]]; then 
+    alias storm='pstorm ./'
+fi
+
+# Where my site is hosted ?
+function hosted() {
+    dig +short -x $(dig +short $1)
+}
