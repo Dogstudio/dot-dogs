19964b2782eb12b54d3a60f4f57592c35b693638
Thierry Lagasse <epagneul@dogstudio.be>
2017-02-13 14:56:44 +0100

    :sparkles: Add script to update machine name.

---
+++ hostchange.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo -e "I'll update your MAC configuration to set your Dogname as Computer...\n"
+
+read -p "What's your dogname: " DOG;
+NEWDOG=`php -r "echo ucfirst(strtolower('$DOG'));"`
+
+sudo -p "Now, enter your connection password (it will not sent or copied, I promise): " scutil --set ComputerName "$NEWDOG" && sudo scutil --set LocalHostName "$NEWDOG" && sudo scutil --set HostName "$NEWDOG" &&\
+echo -e "\Done. You can quit the Terminal, now." || echo -e "\nOups, something went Wrong..."
