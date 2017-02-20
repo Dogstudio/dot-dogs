430277c3aef57ca6731eaed3a15976ffc14a1a0b
Thierry Lagasse <epagneul@dogstudio.be>
2017-02-20 14:14:47 +0100

    :hammer: Refactor Docker script to use with both Docker Toolsbox and Docker for Mac

--- docker.sh
+++ docker.sh
@@ -4,8 +4,10 @@
 #
 #   Add this script to your ".(ba|z)shrc" to add userfriendly aliases
 #
-# -----------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 export DOCKER_PREFIX="\033[4mDOCKER\033[0m -"
+export DOCKER_MACHINE_NAME=""
+export DOCKER_MACHINE_IP=""
 
 COLOR_RED="\033[0;31m"
 COLOR_YELLOW="\033[1;33m"
@@ -17,16 +19,16 @@ COLOR_WHITE="\033[1;37m"
 COLOR_LIGHT_GRAY="\033[0;37m"
 COLOR_NONE="\033[0m"
 
-# -----------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 # Validation
 if [ -z "$(command -v docker)" ]; then
     echo -e "${DOCKER_PREFIX} command \"docker\" ${COLOR_RED}not installed${COLOR_NONE}. Abording.\n"
 fi
 
-# -----------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 # Auto installation
 
-DOCKER_PATH="$HOME/.docker"
+DOCKER_PATH="$HOME/.sctipts"
 DOCKER_SCRIPT="docker_commands.sh"
 DOCKER_URL="https://raw.githubusercontent.com/Dogstudio/docker-dogs/master/scripts/${DOCKER_SCRIPT}"
 
@@ -52,47 +54,131 @@ DOCKER_URL="https://raw.githubusercontent.com/Dogstudio/docker-dogs/master/scrip
     echo -e "${DOCKER_PREFIX} Commands installed ${COLOR_GREEN}successfully${COLOR_NONE}\n"
 )
 
-# -----------------------------------------------------------------------------
+# =================================================================================================
 
+#
+#  (try to) Initialize environment with the current running Docker (machine)
+#
+function _dockerInit()
+{
+    # Docker Machine 
+    if [ -n "$(docker-machine status | grep -i 'run')" ]; then
+        DOCKER_MACHINE_NAME=$(docker-machine ls -q --filter "state=Running")
+        
+        if ! DOCKER_MACHINE_IP=$(docker-machine ip $DOCKER_MACHINE_NAME 2>/dev/null); then 
+            echo -e "${DOCKER_PREFIX} The \"${DOCKER_MACHINE_NAME}\" ${COLOR_RED}has no IP${COLOR_NONE}"; return 1
+        fi
 
+        if ! docker-machine env 2>&1 >/dev/null; then 
+            echo -e "${DOCKER_PREFIX} The \"${DOCKER_MACHINE_NAME}\" ${COLOR_RED}has no ENV${COLOR_NONE}"; return 1
+        fi
+
+        eval $(docker-machine env)
+        echo -e "${DOCKER_PREFIX} Machine \"${DOCKER_MACHINE_NAME}\" is ${COLOR_GREEN}running${COLOR_NONE} with IP : ${DOCKER_MACHINE_IP}"
+
+    # Local Deamon (Docker for Mac)
+    elif docker info 2>/dev/null >/dev/null ; then
+        DOCKER_MACHINE_NAME=$(docker info 2>/dev/null | awk -F':' '/Name/ {print $2}' | tr -d ' ')
+        DOCKER_MACHINE_IP="127.0.0.1"
+        
+        echo -e "${DOCKER_PREFIX} Local Docker deamon is ${COLOR_GREEN}running${COLOR_NONE}."
+    
+    # None is running
+    else
+        echo -e "${DOCKER_PREFIX} Docker ${COLOR_RED} is not running${COLOR_NONE}"; return 1
+    fi
+
+    export DOCKER_MACHINE_NAME
+    export DOCKER_MACHINE_IP
+}
+
+#
+# Initialize the terminal Session
+#
+function _dockerSessionInit() 
+{
+    _dockerGenericAlias
+    _dockerInit
+    _dockerAlias
+    _dockerCompleter
+    _updateDnsMask
+}
+
+#
+# Create Aliases for common use
+#
+function _dockerGenericAlias()
+{
+    alias doma="docker-machine"
+    alias doco="docker-compose"
+}
+
+#
+# Create alias for daily use commands
+#
 function _dockerAlias()
 {
-    alias doco="docker-compose"
-
     alias doup="docker-compose build && docker-compose up -d --remove-orphans"
     alias dodown="docker-compose stop"
     alias doreload="dodown && doup"
     alias dologs="docker-compose logs"
-    alias dostop='docker stop $(docker ps -q)'
     alias doall="docker-compose down && docker-compose rm && docker-compose build --no-cache --force-rm && docker-compose up -d"
+    alias doinit="_dockerInit"
 }
 
-# -------------------------------------------------------------------------
-
-function _dockerInit()
+#
+#   Autocompleter
+#
+function _dockerCompleter()
 {
-    if ! docker info 2>/dev/null >/dev/null ; then
-        echo -e "${DOCKER_PREFIX} Local deamon is ${COLOR_RED}not running${COLOR_NONE}\n"; return 1
+    if $(type complete >/dev/null); then
+        
+        # Search for Container
+        function _completeContainer()
+        {
+            local word="${COMP_WORDS[COMP_CWORD]}"
+            COMPREPLY=( $(compgen -W "$(docker ps --filter "status=running" --format "{{.Names}}")" -- "$word") )
+        }
+        complete -F _completeContainer doshell
+        complete -F _completeContainer dologs
+
+        # Docker machine
+        if [ -n "$(command -v docker-machine)" ]; then
+            function _completeDockerMachine()
+            {
+                local cmd="${1##*/}"
+                local word="${COMP_WORDS[COMP_CWORD]}"
+
+                case "$cmd" in
+                    dostart)
+                       COMPREPLY=( $(compgen -W "$(docker-machine ls -q --filter "state=Stopped")" -- "$word") )
+                       ;;
+                    dostop)
+                       COMPREPLY=( $(compgen -W "$(docker-machine ls -q --filter "state=Running")" -- "$word") )
+                       ;;
+                    *)
+                        COMPREPLY=( $(compgen -W "active config create env inspect ip kill ls provision restart rm ssh scp start status stop upgrade url version help regenerate-certs" -- "$word") )
+                        ;;
+                esac
+            }
+            complete -F _completeDockerMachine doma
+            complete -F _completeDockerMachine docker-machine
+            complete -F _completeDockerMachine dostart 
+            complete -F _completeDockerMachine dostop 
+        fi
     fi
-
-    DOCKER_MACHINE_NAME=$(docker info 2>/dev/null | awk -F':' '/Name/ {print $2}' | tr -d ' ')
-    echo -e "${DOCKER_PREFIX} Local deamon \"${DOCKER_MACHINE_NAME}\" is ${COLOR_GREEN}running${COLOR_NONE}.\n"
-
-    _dockerAlias
-    _updateDnsMask
 }
 
-# -------------------------------------------------------------------------
-
-# Update and restart dnsmasq (if present)
+#
+# Update and restart dnsmasq (if present) with the current IP
+#
 function _updateDnsMask()
 {
     DNSMASQ_CONF="$(brew --prefix)/etc/dnsmasq.conf"
-    DOCKER_MACHINE_IP='127.0.0.1'
-
+    
     if [ -f ${DNSMASQ_CONF} ]; then
         if ! grep "${DOCKER_MACHINE_IP}" ${DNSMASQ_CONF} >/dev/null ; then
-            echo -e "${DOCKER_PREFIX} I need ${COLOR_BLUE}update dnsmasq${COLOR_NONE} to match IP: ${DOCKER_MACHINE_IP}."
+            echo -e "${DOCKER_PREFIX} I need ${COLOR_RED}update dnsmasq to match IP to \"${DOCKER_MACHINE_IP}\"${COLOR_NONE}."
             sed -i -e "s|dok/[0-9.]*$|dok/${DOCKER_MACHINE_IP}|" ${DNSMASQ_CONF}
 
             sudo launchctl stop homebrew.mxcl.dnsmasq &&
@@ -102,7 +188,20 @@ function _updateDnsMask()
     fi
 }
 
-# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
+
+# Start a machine
+function dostart()
+{
+    docker-machine start $1 && _dockerInit && _updateDnsMask
+}
+
+# Stop a machine
+function dostop()
+{
+    docker-machine stop $1 && _dockerInit
+}
+
 
 # Run a shell on the specified container
 function doshell()
@@ -110,35 +209,23 @@ function doshell()
     docker exec -t -i $1 /bin/bash
 }
 
-
-# -------------------------------------------------------------------------
-
 function dohelp()
 {
-    echo -e "${DOCKER_PREFIX} Helper Commands.\n"
-
-    echo -e "  ${COLOR_BLUE}doup${COLOR_NONE} : Build and Up the current Docker compose."
-    echo -e "  ${COLOR_BLUE}dodown${COLOR_NONE} : Down the current Docker compose."
-    echo -e "  ${COLOR_BLUE}doreload${COLOR_NONE} : Down and up the current Docker compose."
-    echo -e "  ${COLOR_BLUE}doshell${COLOR_NONE} : Open shell on specified container (autocomplete)."
-    echo -e "  ${COLOR_BLUE}dologs${COLOR_NONE} : Start the Logging system for the current Docker compose."
-
-    echo -e "  ${COLOR_WHITE}doco${COLOR_NONE} : Simple alias for docker-compose."
+    echo -e "${DOCKER_PREFIX} Helper Commands."
+    echo ''
+    echo -e "  ${COLOR_WHITE}doma${COLOR_NONE}:     Simple alias for docker-machine."
+    echo -e "  ${COLOR_WHITE}doco${COLOR_NONE}:     Simple alias for docker-compose."
+    echo -e "  ${COLOR_WHITE}doinit${COLOR_NONE}:   Initialize the session according the Docker ENV."
+    echo ''
+    echo -e "  ${COLOR_BLUE}doup${COLOR_NONE}:     Build and Up the current Docker compose."
+    echo -e "  ${COLOR_BLUE}dodown${COLOR_NONE}:   Down the current Docker compose."
+    echo -e "  ${COLOR_BLUE}doreload${COLOR_NONE}: Down and up the current Docker compose."
+    echo -e "  ${COLOR_BLUE}doshell${COLOR_NONE}:  Open shell on specified container (autocomplete)."
+    echo -e "  ${COLOR_BLUE}dologs${COLOR_NONE}:   Start the Logging system for the current Docker compose."
 }
 
-# -------------------------------------------------------------------------
-# Autocompleter
+# -------------------------------------------------------------------------------------------------
 
-if $(type complete >/dev/null); then
-    function _completecontainer()
-    {
-        local word="${COMP_WORDS[COMP_CWORD]}"
-        COMPREPLY=( $(compgen -W "$(docker ps --filter status=running --format "{{.Names}}")" -- "$word") )
-    }
-    complete -F _completecontainer doshell
-	complete -F _completecontainer dologs
-fi
-
-# -------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 # Init
-_dockerInit
+_dockerSessionInit
